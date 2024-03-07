33127162760fed0b3cf85ecc80c2ebb16d7c9aa7
zul1996 <mzulnewbie@gmail.com>
2024-03-07 11:26:28 +0700

    Update linktree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,103 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { CiLinkedin, CiTwitter } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <main className=" bg-primary   flex items-center justify-center h-screen">
+      <div className=" absolute  top-[2%] lg:top-[11%]  z-10 ">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/Monsters.svg"
+          alt="phone"
+          width={400}
+          height={400}
+          className="rounded-full"
         />
       </div>
+      <div className="z-20 bg-background border-solid border-primary-foreground border-4 p-10 rounded-lg lg:flex lg:flex-row  lg:items-center lg:justify-between lg:gap-28  ">
+        <div className="flex flex-col items-center">
+          <Image
+            src="/profile.jpeg"
+            alt="phone"
+            width={200}
+            height={300}
+            className="rounded-full"
+          />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className="mt-5 text-2xl font-bold ">
+            Muhammad Zulkarnaen Indranto
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <h2 className="mt-1 text-slate-400"> Full stack Developer</h2>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="mt-5 gap-8 flex flex-col">
+            <div className="">
+              <Link
+                href="/cv.pdf"
+                download="cv"
+                passHref={true}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <Button className=" w-[400px]" variant={"default"}>
+                  cv
+                </Button>
+              </Link>
+            </div>
+            <div>
+              <Link
+                href="https://stackoverflow.com/"
+                passHref={true}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <Button className="w-[400px] " size={"lg"} variant={"default"}>
+                  Email
+                </Button>
+              </Link>
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <Link
+              href="https://stackoverflow.com/"
+              passHref={true}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Button className="w-[400px]" variant={"default"}>
+                Whatsapp
+              </Button>
+            </Link>
+          </div>
+          <div className="flex mt-8 gap-3">
+            <Link
+              href="https://www.instagram.com/zul_indr/"
+              passHref={true}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <FaInstagram className="size-8 " />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/muhammad-zulkarnaen-indranto-583151174/"
+              passHref={true}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <CiLinkedin className="size-8" />
+            </Link>
+            <Link
+              href="https://twitter.com/Flaatena_"
+              passHref={true}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <CiTwitter className="size-8" />
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
